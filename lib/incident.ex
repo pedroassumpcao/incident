a386ec57d7ffa112aa85a9779ecbdf83b1de82af
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-05-19 22:14:49 -0500

    Add event handler to update projections.

--- lib/incident.ex
+++ lib/incident.ex
@@ -20,162 +20,10 @@ defmodule Incident.Event.PersistedEvent do
   defstruct [:event_id, :aggregate_id, :event_type, :version, :event_date, :event_data]
 end
 
-# Event Store Adapter
-defmodule Incident.Event.Store.Adapter do
-  @callback get(String.t()) :: list
-  @callback append(map) :: :ok
-end
+# Projection
 
-defmodule Incident.Event.Store.InMemoryAdapter do
-  @behaviour Incident.Event.Store.Adapter
-
-  use Agent
-
-  alias Incident.Event.PersistedEvent
-
-  def start_link(initial_value) do
-    Agent.start_link(fn -> initial_value end, name: __MODULE__)
-  end
-
-  @impl true
-  def get(aggregate_id) do
-    __MODULE__
-    |> Agent.get(& &1)
-    |> Enum.filter(&(&1.aggregate_id == aggregate_id))
-    |> Enum.reverse()
-  end
-
-  @impl true
-  def append(event) do
-    persisted_event = %PersistedEvent{
-      event_id: :rand.uniform(100_000) |> Integer.to_string(),
-      aggregate_id: event.aggregate_id,
-      event_type: event.__struct__ |> Module.split() |> List.last(),
-      version: event.version,
-      event_date: DateTime.utc_now(),
-      event_data: Map.from_struct(event)
-    }
-
-    Agent.update(__MODULE__, &([persisted_event] ++ &1))
-  end
-end
-
-# Event Store
-defmodule Incident.Event.Store do
-  def get(aggregate_id), do: adapter().get(aggregate_id)
-
-  def append(event), do: adapter().append(event)
-
-  defp adapter do
-    Incident.Event.Store.InMemoryAdapter
-  end
-end
-
-# Aggregate State
-defmodule Incident.AggregateState do
-  defmacro __using__(opts) do
-    aggregate = Keyword.get(opts, :aggregate)
-    initial_state = Keyword.get(opts, :initial_state)
-
-    quote do
-      import Incident.AggregateState
-
-      alias Incident.Event.Store
-
-      def get(aggregate_id) do
-        aggregate_id
-        |> Store.get()
-        |> Enum.reduce(unquote(initial_state), fn event, state ->
-          unquote(aggregate).apply(event, state)
-        end)
-      end
-    end
-  end
-end
-
-defmodule Incident.BankAccountState do
-  use Incident.AggregateState,
-    aggregate: Incident.BankAccount,
-    initial_state: %{
-      aggregate_id: nil,
-      account_number: nil,
-      balance: nil,
-      version: nil,
-      updated_at: nil
-    }
-end
-
-# Aggregate
-defmodule Incident.Aggregate do
-  @callback execute(struct) :: :ok | {:error, atom}
-  @callback apply(struct, map) :: map
-end
-
-defmodule Incident.BankAccount do
-  @behaviour Incident.Aggregate
-
-  alias Incident.BankAccountState
-  alias Incident.Command.{DepositMoney, OpenAccount}
-  alias Incident.Event.{AccountOpened, MoneyDeposited, Store}
-
-  @impl true
-  def execute(%OpenAccount{account_number: account_number}) do
-    case BankAccountState.get(account_number) do
-      %{account_number: nil} ->
-        %AccountOpened{
-          aggregate_id: account_number,
-          account_number: account_number,
-          version: 1
-        }
-        |> Store.append()
-
-      _ ->
-        {:error, :account_already_opened}
-    end
-  end
-
-  @impl true
-  def execute(%DepositMoney{aggregate_id: aggregate_id, amount: amount}) do
-    case BankAccountState.get(aggregate_id) do
-      %{aggregate_id: aggregate_id} = state when not is_nil(aggregate_id) ->
-        %MoneyDeposited{
-          aggregate_id: aggregate_id,
-          amount: amount,
-          version: state.version + 1
-        }
-        |> Store.append()
-
-      _ ->
-        {:error, :account_not_found}
-    end
-  end
-
-  @impl true
-  def apply(%{event_type: "AccountOpened"} = event, state) do
-    %{
-      state
-      | aggregate_id: event.aggregate_id,
-        account_number: event.event_data.account_number,
-        balance: 0,
-        version: event.version,
-        updated_at: event.event_date
-    }
-  end
-
-  @impl true
-  def apply(%{event_type: "MoneyDeposited"} = event, state) do
-    %{
-      state
-      | balance: state.balance + event.event_data.amount,
-        version: event.version,
-        updated_at: event.event_date
-    }
-  end
-
-  @impl true
-  def apply(_, state) do
-    state
-  end
+defmodule Incident.Projection.BankAccount do
+  defstruct [:aggregate_id, :account_number, :version, :balance, :event_id, :event_date]
 end
 
 # Projection Store Adapter
@@ -233,5 +81,212 @@ defmodule Incident.Projection.Store do
 
   defp adapter do
     Incident.Projection.Store.InMemoryAdapter
+  end
+end
+
+# Event Handler
+defmodule Incident.EventHandler do
+  alias Incident.Event.PersistedEvent
+  alias Incident.Projection.Store
+  alias Incident.Projection.BankAccount
+  alias Incident.BankAccount, as: Aggregate
+
+  def listen(%PersistedEvent{event_type: "AccountOpened"} = event, state) do
+    new_state = Aggregate.apply(event, state)
+    data = %BankAccount{
+      aggregate_id: new_state.aggregate_id,
+      account_number: new_state.account_number,
+      balance: new_state.balance,
+      version: event.version,
+      event_id: event.event_id,
+      event_date: event.event_date
+    }
+    Store.project(:bank_accounts, data)
+  end
+
+  def listen(%PersistedEvent{event_type: "MoneyDeposited"} = event, state) do
+    new_state = Aggregate.apply(event, state)
+    data = %BankAccount{
+      aggregate_id: new_state.aggregate_id,
+      account_number: new_state.account_number,
+      balance: new_state.balance,
+      version: event.version,
+      event_id: event.event_id,
+      event_date: event.event_date
+    }
+    Store.project(:bank_accounts, data)
+  end
+end
+
+# Event Store Adapter
+defmodule Incident.Event.Store.Adapter do
+  @callback get(String.t()) :: list
+  @callback append(map) :: :ok
+end
+
+defmodule Incident.Event.Store.InMemoryAdapter do
+  @behaviour Incident.Event.Store.Adapter
+
+  use Agent
+
+  alias Incident.Event.PersistedEvent
+
+  def start_link(initial_value) do
+    Agent.start_link(fn -> initial_value end, name: __MODULE__)
+  end
+
+  @impl true
+  def get(aggregate_id) do
+    __MODULE__
+    |> Agent.get(& &1)
+    |> Enum.filter(&(&1.aggregate_id == aggregate_id))
+    |> Enum.reverse()
+  end
+
+  @impl true
+  def append(event) do
+    persisted_event = %PersistedEvent{
+      event_id: :rand.uniform(100_000) |> Integer.to_string(),
+      aggregate_id: event.aggregate_id,
+      event_type: event.__struct__ |> Module.split() |> List.last(),
+      version: event.version,
+      event_date: DateTime.utc_now(),
+      event_data: Map.from_struct(event)
+    }
+
+    Agent.update(__MODULE__, &([persisted_event] ++ &1))
+
+    {:ok, persisted_event}
+  end
+end
+
+# Event Store
+defmodule Incident.Event.Store do
+  def get(aggregate_id) do
+    apply(adapter(), :get, [aggregate_id])
+  end
+
+  def append(event) do
+    apply(adapter(), :append, [event])
+  end
+
+  defp adapter do
+    Incident.Event.Store.InMemoryAdapter
+  end
+end
+
+# Aggregate State
+defmodule Incident.AggregateState do
+  defmacro __using__(opts) do
+    aggregate = Keyword.get(opts, :aggregate)
+    initial_state = Keyword.get(opts, :initial_state)
+
+    quote do
+      import Incident.AggregateState
+
+      alias Incident.Event.Store
+
+      def get(aggregate_id) do
+        aggregate_id
+        |> Store.get()
+        |> Enum.reduce(unquote(initial_state), fn event, state ->
+          unquote(aggregate).apply(event, state)
+        end)
+      end
+    end
+  end
+end
+
+defmodule Incident.BankAccountState do
+  use Incident.AggregateState,
+    aggregate: Incident.BankAccount,
+    initial_state: %{
+      aggregate_id: nil,
+      account_number: nil,
+      balance: nil,
+      version: nil,
+      updated_at: nil
+    }
+end
+
+# Aggregate
+defmodule Incident.Aggregate do
+  @callback execute(struct) :: :ok | {:error, atom}
+  @callback apply(struct, map) :: map
+end
+
+defmodule Incident.BankAccount do
+  @behaviour Incident.Aggregate
+
+  alias Incident.BankAccountState
+  alias Incident.Command.{DepositMoney, OpenAccount}
+  alias Incident.Event.{AccountOpened, MoneyDeposited, Store}
+
+  @impl true
+  def execute(%OpenAccount{account_number: account_number}) do
+    case BankAccountState.get(account_number) do
+      %{account_number: nil} = state ->
+        %AccountOpened{
+          aggregate_id: account_number,
+          account_number: account_number,
+          version: 1
+        }
+        |> Store.append()
+        |> case do
+             {:ok, persisted_event} -> Incident.EventHandler.listen(persisted_event, state)
+             error -> error
+           end
+
+      _ ->
+        {:error, :account_already_opened}
+    end
+  end
+
+  @impl true
+  def execute(%DepositMoney{aggregate_id: aggregate_id, amount: amount}) do
+    case BankAccountState.get(aggregate_id) do
+      %{aggregate_id: aggregate_id} = state when not is_nil(aggregate_id) ->
+        %MoneyDeposited{
+          aggregate_id: aggregate_id,
+          amount: amount,
+          version: state.version + 1
+        }
+        |> Store.append()
+        |> case do
+             {:ok, persisted_event} -> Incident.EventHandler.listen(persisted_event, state)
+             error -> error
+           end
+
+
+      _ ->
+        {:error, :account_not_found}
+    end
+  end
+
+  @impl true
+  def apply(%{event_type: "AccountOpened"} = event, state) do
+    %{
+      state
+      | aggregate_id: event.aggregate_id,
+        account_number: event.event_data.account_number,
+        balance: 0,
+        version: event.version,
+        updated_at: event.event_date
+    }
+  end
+
+  @impl true
+  def apply(%{event_type: "MoneyDeposited"} = event, state) do
+    %{
+      state
+      | balance: state.balance + event.event_data.amount,
+        version: event.version,
+        updated_at: event.event_date
+    }
+  end
+
+  @impl true
+  def apply(_, state) do
+    state
   end
 end
