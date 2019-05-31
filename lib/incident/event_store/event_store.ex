aa622de7d3f7b15ac422468ed1f674eec4ae65f7
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-05-31 16:40:11 -0500

    Add documentation for event store.

--- lib/incident/event_store/event_store.ex
+++ lib/incident/event_store/event_store.ex
@@ -1,12 +1,21 @@
 defmodule Incident.EventStore do
+  @moduledoc """
+  Defines the API to interact with the Event Store.
+
+  The data source is based on the configured Event Store Adapter.
+  """
+
+  @doc false
   def get(aggregate_id) do
     apply(adapter(), :get, [aggregate_id])
   end
 
+  @doc false
   def append(event) do
     apply(adapter(), :append, [event])
   end
 
+  @spec adapter :: module
   defp adapter do
     Incident.EventStore.InMemoryAdapter
   end
