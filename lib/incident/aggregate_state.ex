df7c7befadb145f6bc088e43843b81254d36bf00
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-05-31 16:39:26 -0500

    Add documentation for aggregate state.

--- lib/incident/aggregate_state.ex
+++ lib/incident/aggregate_state.ex
@@ -1,4 +1,8 @@
 defmodule Incident.AggregateState do
+  @moduledoc """
+  Defines an Aggregate State macro that receives the aggregate module and an initial state.
+  """
+
   defmacro __using__(opts) do
     aggregate = Keyword.get(opts, :aggregate)
     initial_state = Keyword.get(opts, :initial_state)
@@ -8,6 +12,10 @@ defmodule Incident.AggregateState do
 
       alias Incident.EventStore
 
+      @doc """
+      Reads all the persisted events for a specific aggregate and apply them, returning the current aggregate state.
+      """
+      @spec get(String.t()) :: any
       def get(aggregate_id) do
         aggregate_id
         |> EventStore.get()
