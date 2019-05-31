fc7e89a4f72a9cbe6449b4f97e26a4607b4d3bea
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-05-31 16:41:14 -0500

    Add documentation for projection store.

--- lib/incident/projection_store/projection_store.ex
+++ lib/incident/projection_store/projection_store.ex
@@ -1,12 +1,21 @@
 defmodule Incident.ProjectionStore do
+  @moduledoc """
+  Defines the API to interact with the Projection Store.
+
+  The data source is based on the configured Projection Store Adapter.
+  """
+
+  @doc false
   def project(projection_name, data) do
     apply(adapter(), :project, [projection_name, data])
   end
 
+  @doc false
   def all(projection_name) do
     apply(adapter(), :all, [projection_name])
   end
 
+  @spec adapter :: module
   defp adapter do
     Incident.ProjectionStore.InMemoryAdapter
   end
