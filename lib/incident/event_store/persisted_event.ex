4ef63efb0e69345a91f1ee43f2aeb224fd6d16c4
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-05-31 16:40:31 -0500

    Add documentation for persisted event data structure.

--- lib/incident/event_store/persisted_event.ex
+++ lib/incident/event_store/persisted_event.ex
@@ -1,3 +1,10 @@
 defmodule Incident.Event.PersistedEvent do
+  @moduledoc """
+  Defines the common data structure for any event that is persisted in the Event Store.
+
+  All fields are required.
+  """
+
+  @enforce_keys [:event_id, :aggregate_id, :event_type, :version, :event_date, :event_data]
   defstruct [:event_id, :aggregate_id, :event_type, :version, :event_date, :event_data]
 end
