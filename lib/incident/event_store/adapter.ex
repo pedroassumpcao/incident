5a7f9d1c27cf8ce3d8b808869a815b7f17eb83de
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-05-31 16:39:52 -0500

    Add documentation for event store adapter.

--- lib/incident/event_store/adapter.ex
+++ lib/incident/event_store/adapter.ex
@@ -1,4 +1,15 @@
 defmodule Incident.EventStore.Adapter do
+  @moduledoc """
+  Defines the API for an Event Store adapter.
+  """
+
+  @doc """
+  Receives an aggregate id and returns a list containing all persisted events from the Event Store.
+  """
   @callback get(String.t()) :: list
+
+  @doc """
+  Appends an event to the Event Store.
+  """
   @callback append(map) :: :ok
 end
