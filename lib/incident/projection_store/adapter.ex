2ea3acc6e142df0be230240cee90240f6935c87f
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-05-31 16:40:50 -0500

    Add documentation for projection store adapter.

--- lib/incident/projection_store/adapter.ex
+++ lib/incident/projection_store/adapter.ex
@@ -1,4 +1,17 @@
 defmodule Incident.ProjectionStore.Adapter do
+  @moduledoc """
+  Defines the API for a Projection Store adapter.
+  """
+
+  @doc """
+  Insert or updates a projection in the Projection Store.
+
+  Receives the projection type and the data for the projection.
+  """
   @callback project(:atom, map) :: map
+
+  @doc """
+  Returns all projections from a specific projection type from the Projection Store.
+  """
   @callback all(:atom) :: list
 end
