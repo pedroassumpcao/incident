38b5498fa3a3f44090bd7d04ebffe687d169f1f3
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-05-30 22:25:07 -0500

    Add credo as dependency.

--- mix.exs
+++ mix.exs
@@ -11,7 +11,6 @@ defmodule Incident.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -19,11 +18,9 @@ defmodule Incident.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 end
