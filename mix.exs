db73a57ef7eceb2680f240e8b68957f7a27af823
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-06-01 09:52:15 -0500

    Add ecto as dependency for commands, events, projections data structs.

--- mix.exs
+++ mix.exs
@@ -43,6 +43,7 @@ defmodule Incident.MixProject do
     [
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
+      {:ecto, "~> 3.1.4"},
       {:ex_doc, "~> 0.20.2", only: :dev, runtime: false},
       {:excoveralls, "~> 0.11.1", only: :test}
     ]
