f2f5e653fe715943e97542cce3946788bd3c5c38
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-06-01 18:10:48 -0500

    Define persisted event as ecto embedded schema.

--- lib/incident/event_store/persisted_event.ex
+++ lib/incident/event_store/persisted_event.ex
@@ -6,14 +6,34 @@ defmodule Incident.Event.PersistedEvent do
   """
 
   @type t :: %__MODULE__{
-          event_id: String.t(),
-          aggregate_id: String.t(),
-          event_type: String.t(),
-          version: pos_integer,
-          event_date: DateTime.t(),
-          event_data: map
+          event_id: String.t() | nil,
+          aggregate_id: String.t() | nil,
+          event_type: String.t() | nil,
+          version: pos_integer | nil,
+          event_date: DateTime.t() | nil,
+          event_data: map | nil
         }
 
-  @enforce_keys [:event_id, :aggregate_id, :event_type, :version, :event_date, :event_data]
-  defstruct [:event_id, :aggregate_id, :event_type, :version, :event_date, :event_data]
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key false
+  embedded_schema do
+    field(:event_id, :string)
+    field(:aggregate_id, :string)
+    field(:event_type, :string)
+    field(:version, :integer)
+    field(:event_date, :utc_datetime)
+    field(:event_data, :map)
+  end
+
+  @required_fields ~w(event_id aggregate_id event_type version event_date event_data)a
+
+  @doc false
+  @spec changeset(t, map) :: Ecto.Changeset.t()
+  def changeset(record, params \\ %{}) do
+    record
+    |> cast(params, @required_fields)
+    |> validate_required(@required_fields)
+  end
 end
---
+++ test/incident/events/persisted_event_test.exs
@@ -0,0 +1,43 @@
+defmodule Incident.Event.PersistedEventTest do
+  use Incident.DataCase, async: true
+
+  alias Ecto.UUID
+  alias Incident.Event.PersistedEvent
+
+  @valid_params %{
+    event_id: UUID.generate(),
+    aggregate_id: UUID.generate(),
+    event_type: "SomethingHappened",
+    version: 1,
+    event_date: DateTime.utc_now(),
+    event_data: %{}
+  }
+
+  describe "changeset/2" do
+    test "returns a valid changeset when all fields are valid" do
+      changeset = PersistedEvent.changeset(%PersistedEvent{}, @valid_params)
+
+      assert changeset.valid?
+    end
+
+    test "returns an error when a required field is not present" do
+      changeset = PersistedEvent.changeset(%PersistedEvent{}, %{})
+
+      refute changeset.valid?
+      assert %{event_id: ["can't be blank"]} = errors_on(changeset)
+      assert %{aggregate_id: ["can't be blank"]} = errors_on(changeset)
+      assert %{event_type: ["can't be blank"]} = errors_on(changeset)
+      assert %{event_date: ["can't be blank"]} = errors_on(changeset)
+      assert %{event_data: ["can't be blank"]} = errors_on(changeset)
+      assert %{version: ["can't be blank"]} = errors_on(changeset)
+    end
+
+    test "returns an error when a field is set with a wront type" do
+      invalid_params = Map.merge(@valid_params, %{event_date: "2010-04-11"})
+      changeset = PersistedEvent.changeset(%PersistedEvent{}, invalid_params)
+
+      refute changeset.valid?
+      assert %{event_date: ["is invalid"]} = errors_on(changeset)
+    end
+  end
+end
