6e7cc4e92edbc7810d6ee8fb53540b9189b84ba9
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-06-03 17:00:01 -0500

    Refactor how config for adapters are defined.

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,13 @@
 use Mix.Config
 
-config :incident, :event_store, adapter: Incident.EventStore.InMemoryAdapter, initial_state: []
+config :incident, :event_store,
+  adapter: Incident.EventStore.InMemoryAdapter,
+  options: %{
+    initial_state: []
+  }
 
 config :incident, :projection_store,
   adapter: Incident.ProjectionStore.InMemoryAdapter,
-  initial_state: %{}
+  options: %{
+    initial_state: %{}
+  }
--- lib/incident/application.ex
+++ lib/incident/application.ex
@@ -5,12 +5,30 @@ defmodule Incident.Application do
 
   def start(_type, _args) do
     children = [
-      {event_store_config()[:adapter], event_store_config()[:initial_state]},
-      {projection_store_config()[:adapter], projection_store_config()[:initial_state]}
+      {event_store_adapter(), event_store_options()},
+      {projection_store_adapter(), projection_store_options()}
     ]
 
     opts = [strategy: :one_for_one, name: Incident.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp event_store_adapter do
+    event_store_config()[:adapter] ||
+      raise "An Event Store adapter is required in the config."
+  end
+
+  defp event_store_options do
+    event_store_config()[:options][:initial_state]
+  end
+
+  defp projection_store_adapter do
+    projection_store_config()[:adapter] ||
+      raise "A Projection Store adapter is required in the config."
+  end
+
+  defp projection_store_options do
+    projection_store_config()[:options][:initial_state]
   end
 
   @spec event_store_config :: keyword
