6b67ef4911db16fc2a5c799d744f636761bf6bca
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-06-13 18:13:02 -0500

    Complement more tests for bank example app.

--- examples/bank/test/bank_test.exs
+++ examples/bank/test/bank_test.exs
@@ -5,22 +5,72 @@ defmodule BankTest do
   alias Bank.Commands.{DepositMoney, OpenAccount}
   alias Ecto.UUID
 
-  test "executes valid commands" do
-    command_open_account = %OpenAccount{account_number: UUID.generate()}
-    assert :ok = BankAccount.execute(command_open_account)
+  setup do
+    on_exit(fn ->
+      :ok = Application.stop(:incident)
+
+      {:ok, _apps} = Application.ensure_all_started(:incident)
+    end)
+  end
+
+  @account_number UUID.generate()
+  @command_open_account %OpenAccount{account_number: @account_number}
+  @command_deposit_money %DepositMoney{aggregate_id: @account_number, amount: 100}
+
+  test "executes an open account command" do
+    assert :ok = BankAccount.execute(@command_open_account)
+
+    assert [bank_account] = Incident.ProjectionStore.all(:bank_accounts)
+    assert bank_account.aggregate_id == @account_number
+    assert bank_account.account_number == @account_number
+    assert bank_account.balance == 0
+    assert bank_account.version == 1
+    assert bank_account.event_date
+    assert bank_account.event_id
   end
 
   test "returns an error when executes an invalid command" do
-    command_open_account = %OpenAccount{account_number: UUID.generate()}
-    assert :ok = BankAccount.execute(command_open_account)
-    assert {:error, :account_already_opened} = BankAccount.execute(command_open_account)
+    assert :ok = BankAccount.execute(@command_open_account)
+    assert {:error, :account_already_opened} = BankAccount.execute(@command_open_account)
+
+    assert [bank_account] = Incident.ProjectionStore.all(:bank_accounts)
+    assert bank_account.aggregate_id == @account_number
+    assert bank_account.account_number == @account_number
+    assert bank_account.balance == 0
+    assert bank_account.version == 1
+    assert bank_account.event_date
+    assert bank_account.event_id
   end
 
-  test "executes multiple valid commands" do
-    account_number = UUID.generate()
-    command_open_account = %OpenAccount{account_number: account_number}
-    assert :ok = BankAccount.execute(command_open_account)
-    command_deposit_money = %DepositMoney{aggregate_id: account_number, amount: 100}
-    assert :ok = BankAccount.execute(command_deposit_money)
+  test "executes an open account and deposit money commands" do
+    assert :ok = BankAccount.execute(@command_open_account)
+
+    assert [bank_account] = Incident.ProjectionStore.all(:bank_accounts)
+    assert bank_account.aggregate_id == @account_number
+    assert bank_account.account_number == @account_number
+    assert bank_account.balance == 0
+    assert bank_account.version == 1
+    assert bank_account.event_date
+    assert bank_account.event_id
+
+    assert :ok = BankAccount.execute(@command_deposit_money)
+
+    assert [bank_account] = Incident.ProjectionStore.all(:bank_accounts)
+    assert bank_account.aggregate_id == @account_number
+    assert bank_account.account_number == @account_number
+    assert bank_account.balance == 100
+    assert bank_account.version == 2
+    assert bank_account.event_date
+    assert bank_account.event_id
+
+    assert :ok = BankAccount.execute(@command_deposit_money)
+
+    assert [bank_account] = Incident.ProjectionStore.all(:bank_accounts)
+    assert bank_account.aggregate_id == @account_number
+    assert bank_account.account_number == @account_number
+    assert bank_account.balance == 200
+    assert bank_account.version == 3
+    assert bank_account.event_date
+    assert bank_account.event_id
   end
 end
