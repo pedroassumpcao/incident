526a4671035aa653bb3612c980d5d13196fc893f
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-06-14 16:48:36 -0500

    Complement tests in the bank example app.

--- examples/bank/test/bank_test.exs
+++ examples/bank/test/bank_test.exs
@@ -20,7 +20,17 @@ defmodule BankTest do
   test "executes an open account command" do
     assert :ok = BankAccount.execute(@command_open_account)
 
+    assert [event] = Incident.EventStore.get(@account_number)
+
+    assert event.aggregate_id == @account_number
+    assert event.event_type == "AccountOpened"
+    assert event.event_id
+    assert event.event_date
+    assert is_map(event.event_data)
+    assert event.version == 1
+
     assert [bank_account] = Incident.ProjectionStore.all(:bank_accounts)
+
     assert bank_account.aggregate_id == @account_number
     assert bank_account.account_number == @account_number
     assert bank_account.balance == 0
@@ -29,11 +39,21 @@ defmodule BankTest do
     assert bank_account.event_id
   end
 
-  test "returns an error when executes an invalid command" do
+  test "invalid commands don't generate new events" do
     assert :ok = BankAccount.execute(@command_open_account)
     assert {:error, :account_already_opened} = BankAccount.execute(@command_open_account)
 
+    assert [event] = Incident.EventStore.get(@account_number)
+
+    assert event.aggregate_id == @account_number
+    assert event.event_type == "AccountOpened"
+    assert event.event_id
+    assert event.event_date
+    assert is_map(event.event_data)
+    assert event.version == 1
+
     assert [bank_account] = Incident.ProjectionStore.all(:bank_accounts)
+
     assert bank_account.aggregate_id == @account_number
     assert bank_account.account_number == @account_number
     assert bank_account.balance == 0
@@ -44,28 +64,34 @@ defmodule BankTest do
 
   test "executes an open account and deposit money commands" do
     assert :ok = BankAccount.execute(@command_open_account)
-
-    assert [bank_account] = Incident.ProjectionStore.all(:bank_accounts)
-    assert bank_account.aggregate_id == @account_number
-    assert bank_account.account_number == @account_number
-    assert bank_account.balance == 0
-    assert bank_account.version == 1
-    assert bank_account.event_date
-    assert bank_account.event_id
-
+    assert :ok = BankAccount.execute(@command_deposit_money)
     assert :ok = BankAccount.execute(@command_deposit_money)
 
-    assert [bank_account] = Incident.ProjectionStore.all(:bank_accounts)
-    assert bank_account.aggregate_id == @account_number
-    assert bank_account.account_number == @account_number
-    assert bank_account.balance == 100
-    assert bank_account.version == 2
-    assert bank_account.event_date
-    assert bank_account.event_id
+    assert [event1, event2, event3] = Incident.EventStore.get(@account_number)
 
-    assert :ok = BankAccount.execute(@command_deposit_money)
+    assert event1.aggregate_id == @account_number
+    assert event1.event_type == "AccountOpened"
+    assert event1.event_id
+    assert event1.event_date
+    assert is_map(event1.event_data)
+    assert event1.version == 1
+
+    assert event2.aggregate_id == @account_number
+    assert event2.event_type == "MoneyDeposited"
+    assert event2.event_id
+    assert event2.event_date
+    assert is_map(event2.event_data)
+    assert event2.version == 2
+
+    assert event3.aggregate_id == @account_number
+    assert event3.event_type == "MoneyDeposited"
+    assert event3.event_id
+    assert event3.event_date
+    assert is_map(event3.event_data)
+    assert event3.version == 3
 
     assert [bank_account] = Incident.ProjectionStore.all(:bank_accounts)
+
     assert bank_account.aggregate_id == @account_number
     assert bank_account.account_number == @account_number
     assert bank_account.balance == 200
