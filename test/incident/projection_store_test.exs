f0281f4ac75ead87fb278efebc78b14645b73c7c
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-06-18 16:51:33 -0500

    Add projection store tests.

---
+++ test/incident/projection_store_test.exs
@@ -0,0 +1,52 @@
+defmodule Incident.ProjectionStoreTest do
+  use ExUnit.Case, async: true
+
+  alias Incident.ProjectionStore
+
+  setup do
+    :ok = Application.stop(:incident)
+
+    projection_store_config = [
+      adapter: Incident.ProjectionStore.InMemoryAdapter,
+      options: [
+        initial_state: %{counters: []}
+      ]
+    ]
+
+    Application.put_env(:incident, :projection_store, projection_store_config)
+    {:ok, _apps} = Application.ensure_all_started(:incident)
+
+    on_exit(fn ->
+      :ok = Application.stop(:incident)
+
+      {:ok, _apps} = Application.ensure_all_started(:incident)
+    end)
+  end
+
+  describe "project/2" do
+    test "projects new data into a projection" do
+      assert :ok = ProjectionStore.project(:counters, %{aggregate_id: "123456", amount: 1})
+      assert :ok = ProjectionStore.project(:counters, %{aggregate_id: "123456", amount: 2})
+      assert :ok = ProjectionStore.project(:counters, %{aggregate_id: "123456", amount: 3})
+    end
+  end
+
+  describe "all/1" do
+    test "returns a list of all projections" do
+      assert :ok = ProjectionStore.project(:counters, %{aggregate_id: "123456", amount: 1})
+      assert :ok = ProjectionStore.project(:counters, %{aggregate_id: "789012", amount: 1})
+      assert :ok = ProjectionStore.project(:counters, %{aggregate_id: "789012", amount: 2})
+      assert :ok = ProjectionStore.project(:counters, %{aggregate_id: "123456", amount: 2})
+      assert :ok = ProjectionStore.project(:counters, %{aggregate_id: "789012", amount: 3})
+
+      assert [
+               %{aggregate_id: "123456", amount: 2},
+               %{aggregate_id: "789012", amount: 3}
+             ] = ProjectionStore.all(:counters)
+    end
+
+    test "returns nil if the projection doesn't exist" do
+      refute ProjectionStore.all(:non_existing_projection)
+    end
+  end
+end
