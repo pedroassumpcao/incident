f0106317415956b950a8556eb4403b4e6ab4f15c
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-07-07 09:38:35 -0500

    Use command behaviour in the example app.

--- examples/bank/lib/bank/commands/deposit_money.ex
+++ examples/bank/lib/bank/commands/deposit_money.ex
@@ -1,3 +1,10 @@
 defmodule Bank.Commands.DepositMoney do
+  @behaviour Incident.Command
+
   defstruct [:aggregate_id, :amount]
+
+  @impl true
+  def valid?(_) do
+    true
+  end
 end
--- examples/bank/lib/bank/commands/open_account.ex
+++ examples/bank/lib/bank/commands/open_account.ex
@@ -1,3 +1,10 @@
 defmodule Bank.Commands.OpenAccount do
+  @behaviour Incident.Command
+
   defstruct [:account_number]
+
+  @impl true
+  def valid?(_) do
+    true
+  end
 end
