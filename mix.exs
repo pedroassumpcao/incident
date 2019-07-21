705a5c946ef3311e89af3d0302b1d164057cdd5a
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-07-21 07:45:40 -0500

    Add ecto and postgrex as dependency.

--- mix.exs
+++ mix.exs
@@ -47,8 +47,10 @@ defmodule Incident.MixProject do
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
       {:ecto, "~> 3.1.4"},
+      {:ecto_sql, "~> 3.0"},
       {:ex_doc, "~> 0.20.2", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.11.1", only: :test}
+      {:excoveralls, "~> 0.11.1", only: :test},
+      {:postgrex, ">= 0.0.0"}
     ]
   end
 
