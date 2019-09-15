41042e4432e1a0ebb90731cd9d7210bf9bd6ce8d
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-09-15 17:23:01 -0500

    Change example app databased configuration.

--- examples/bank/config/dev.exs
+++ examples/bank/config/dev.exs
@@ -1,9 +1,16 @@
 use Mix.Config
 
-config :bank, Bank.EventStoreRepo, url: "ecto://postgres:postgres@localhost/bank_event_store_dev"
+config :bank, Bank.EventStoreRepo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "bank_event_store_dev"
 
 config :bank, Bank.ProjectionStoreRepo,
-  url: "ecto://postgres:postgres@localhost/bank_projection_store_dev"
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "bank_projection_store_dev"
 
 config :incident, :event_store,
   adapter: Incident.EventStore.PostgresAdapter,
