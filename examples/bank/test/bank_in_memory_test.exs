31d8699f4fcf665a28c980bc69cf25705d7756fe
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-09-29 17:51:01 -0500

    Add integration tests for money withdraws for Postgres and In Memory adapters.

--- examples/bank/test/bank_in_memory_test.exs
+++ examples/bank/test/bank_in_memory_test.exs
@@ -2,7 +2,7 @@ defmodule BankInMemoryTest do
   use ExUnit.Case
 
   alias Bank.BankAccountCommandHandler
-  alias Bank.Commands.{DepositMoney, OpenAccount}
+  alias Bank.Commands.{DepositMoney, OpenAccount, WithdrawMoney}
   alias Bank.Projections.BankAccount
   alias Ecto.UUID
 
@@ -17,6 +17,7 @@ defmodule BankInMemoryTest do
   @account_number UUID.generate()
   @command_open_account %OpenAccount{account_number: @account_number}
   @command_deposit_money %DepositMoney{aggregate_id: @account_number, amount: 100}
+  @command_withdraw_money %WithdrawMoney{aggregate_id: @account_number, amount: 100}
 
   test "executes an open account command" do
     assert :ok = BankAccountCommandHandler.receive(@command_open_account)
@@ -101,5 +102,78 @@ defmodule BankInMemoryTest do
     assert bank_account.version == 3
     assert bank_account.event_date
     assert bank_account.event_id
+  end
+
+  test "can't deposit money without opening an account" do
+    assert {:error, :account_not_found} =
+             BankAccountCommandHandler.receive(@command_deposit_money)
+  end
+
+  test "executes an open account, deposit and withdraw money commands" do
+    assert :ok = BankAccountCommandHandler.receive(@command_open_account)
+    assert :ok = BankAccountCommandHandler.receive(@command_deposit_money)
+    assert :ok = BankAccountCommandHandler.receive(@command_withdraw_money)
+
+    assert [event1, event2, event3] = Incident.EventStore.get(@account_number)
+
+    assert event1.aggregate_id == @account_number
+    assert event1.event_type == "AccountOpened"
+    assert event1.event_id
+    assert event1.event_date
+    assert is_map(event1.event_data)
+    assert event1.version == 1
+
+    assert event2.aggregate_id == @account_number
+    assert event2.event_type == "MoneyDeposited"
+    assert event2.event_id
+    assert event2.event_date
+    assert is_map(event2.event_data)
+    assert event2.version == 2
+
+    assert event3.aggregate_id == @account_number
+    assert event3.event_type == "MoneyWithdrawn"
+    assert event3.event_id
+    assert event3.event_date
+    assert is_map(event3.event_data)
+    assert event3.version == 3
+
+    assert [bank_account] = Incident.ProjectionStore.all(BankAccount)
+
+    assert bank_account.aggregate_id == @account_number
+    assert bank_account.account_number == @account_number
+    assert bank_account.balance == 0
+    assert bank_account.version == 3
+    assert bank_account.event_date
+    assert bank_account.event_id
+  end
+
+  test "doees not withdraw more money than bank account balance" do
+    assert :ok = BankAccountCommandHandler.receive(@command_open_account)
+
+    assert {:error, :no_enough_balance} =
+             BankAccountCommandHandler.receive(@command_withdraw_money)
+
+    assert [event1] = Incident.EventStore.get(@account_number)
+
+    assert event1.aggregate_id == @account_number
+    assert event1.event_type == "AccountOpened"
+    assert event1.event_id
+    assert event1.event_date
+    assert is_map(event1.event_data)
+    assert event1.version == 1
+
+    assert [bank_account] = Incident.ProjectionStore.all(BankAccount)
+
+    assert bank_account.aggregate_id == @account_number
+    assert bank_account.account_number == @account_number
+    assert bank_account.balance == 0
+    assert bank_account.version == 1
+    assert bank_account.event_date
+    assert bank_account.event_id
+  end
+
+  test "can't withdraw money without opening an account" do
+    assert {:error, :account_not_found} =
+             BankAccountCommandHandler.receive(@command_withdraw_money)
   end
 end
