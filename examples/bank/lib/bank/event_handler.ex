cde7c1bce716f0d7a0810a8904bda5915df6b8d7
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2019-09-29 17:21:37 -0500

    Listen and project money withdrawn event in the event handler.

--- examples/bank/lib/bank/event_handler.ex
+++ examples/bank/lib/bank/event_handler.ex
@@ -36,4 +36,20 @@ defmodule Bank.EventHandler do
 
     ProjectionStore.project(BankAccount, data)
   end
+
+  @impl true
+  def listen(%{event_type: "MoneyWithdrawn"} = event, state) do
+    new_state = Aggregate.apply(event, state)
+
+    data = %{
+      aggregate_id: new_state.aggregate_id,
+      account_number: new_state.account_number,
+      balance: new_state.balance,
+      version: event.version,
+      event_id: event.event_id,
+      event_date: event.event_date
+    }
+
+    ProjectionStore.project(BankAccount, data)
+  end
 end
