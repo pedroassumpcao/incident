81cff34c71fcd13b1293aa636ec5f29026522ca5
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2020-11-15 17:48:06 -0600

    Remove incident no longer needed incident configuration.

--- examples/bank/config/config.exs
+++ examples/bank/config/config.exs
@@ -2,16 +2,4 @@ use Mix.Config
 
 config :bank, ecto_repos: [Bank.EventStoreRepo, Bank.ProjectionStoreRepo]
 
-config :incident, :event_store,
-  adapter: Incident.EventStore.Postgres.Adapter,
-  options: [
-    repo: Bank.EventStoreRepo
-  ]
-
-config :incident, :projection_store,
-  adapter: Incident.ProjectionStore.Postgres.Adapter,
-  options: [
-    repo: Bank.ProjectionStoreRepo
-  ]
-
 import_config "#{Mix.env()}.exs"
