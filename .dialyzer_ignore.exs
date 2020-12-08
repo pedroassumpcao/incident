889cb616842562e16550fa8b1f1308f7ecbd8b3b
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2020-12-08 11:11:29 -0600

    Ignore dialyzer warnings for persistent term.

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,3 +1,5 @@
 [
-  {"lib/mix/tasks/postgres_init.ex", :unused_fun, 107}
+  {"lib/mix/tasks/postgres_init.ex", :unused_fun, 107},
+  {":0:unknown_function Function :persistent_term.get/1 does not exist."},
+  {":0:unknown_function Function :persistent_term.put/2 does not exist."}
 ]
